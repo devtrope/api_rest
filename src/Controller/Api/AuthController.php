c883d46fe0d1a2e47273eaa9d2e6f96eaaefe9a4
Quentin SCHIFFERLE <dev.trope@gmail.com>
2024-07-25 10:12:24 +0200

    feat: Méthode de connexion

---
+++ src/Controller/Api/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthController extends AbstractController
+{
+    #[Route('/api/login', name: 'app_login', methods: ['POST'])]
+    public function login(Request $request, 
+    JWTTokenManagerInterface $JWTManager, 
+    UserPasswordHasherInterface $passwordHasher, 
+    EntityManagerInterface $entityManager): JsonResponse
+    {
+        $credentials = json_decode($request->getContent(), true);
+
+        if (! isset($credentials['email']) || ! isset($credentials['password'])) {
+            return $this->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        $email = $credentials['email'];
+        $password = $credentials['password'];
+
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if (! $user || ! $passwordHasher->isPasswordValid($user, $password)) {
+            return $this->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        return $this->json(['token' => $JWTManager->create($user)]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,21 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
@@ -20,6 +29,13 @@ class AppFixtures extends Fixture
 
             $manager->persist($product);
         }
+
+        $user = new User;
+        $user->setEmail("user@test.com");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+
+        $manager->persist($user);
 
         $manager->flush();
     }
